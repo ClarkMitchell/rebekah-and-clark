26f1085441c717612cdfe429ce13afef4fdee153
Clark Mitchell <ClarkMitchell@users.noreply.github.com>
2019-01-29 20:56:18 -0700

    Make instructions more explicit.

--- components/Rsvp.vue
+++ components/Rsvp.vue
@@ -10,7 +10,7 @@
         RSVP
       </h1>
       <h3 class="subtitle has-text-centered">
-        Text your first and last name to:
+        To RSVP for an individual, Text the first and last name to:
       </h3>
       <h2 class="subtitle has-text-centered">
         <a href="sms:18634174577">863-417-4577</a>
